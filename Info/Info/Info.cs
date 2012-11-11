940695c663a6c5f81b1e279652a9cb125877e0e5
Francisco Molina <jmolina.molina@gmail.com>
2012-11-10 23:25:22 -1200

    Inicio

---
+++ Info/Info/Info.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Management;
+using System.Management.Instrumentation;
+
+namespace Hardware
+{
+    namespace Hardware
+    {
+        class Fan
+        {
+            private ManagementObjectSearcher searcher;
+
+            private bool ActiveCooling;
+            private UInt16 Availability;
+            private string Caption;
+            private UInt32 ConfigManagerErrorCode;
+            private bool ConfigManagerUserConfig;
+            private string CreationClassName;
+            private string Description;
+
+
+            public Fan()
+            {
+
+
+            }
+
+        }
+    }
+}
---
+++ Info/Info/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Management;
+
+namespace ConsoleApplication1
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("Select * from Win32_DiskDrive");
+
+            foreach (ManagementObject cdrom in searcher.Get())
+            {
+                Console.WriteLine("Valor: {0}\n",
+                              cdrom.GetPropertyValue("TotalHeads"));
+            }
+            Console.Read();
+        }
+    }
+}
